75b8ce1cb497340938957ce2e1fdb73d620db70d
ujibashi <ujibashi@fujitsu.com>
2020-04-28 09:56:42 +0900

    Add files via upload

--- run.py
+++ run.py
@@ -5,24 +5,27 @@ import sys
 import re
 from collections import defaultdict
 
-
-
+# check arguments
 if len(sys.argv) == 1:
     print("Usage: python run.py <dir>")
     sys.exit(1)
 
+# get the file list in the given directory
 directory = sys.argv[1]
 files = glob.glob(directory + '/*')
 
+# the dictionary of mail magazines, sur name, given name having the mail address as the keys
 total_dict = defaultdict(set)
 sur_name_dict = defaultdict(str)
 given_name_dict = defaultdict(str)
+
 for fn in files:
+    # extract the mail magazine name
     split_fn = re.split('/', fn)
     filename = split_fn[-1]
     mailmagazine_name = filename[:-4]
 
-    address_dict = {}
+    # read each csv file and store the infos. into the dictionaries
     with open(fn) as fp:
         reader = csv.reader(fp)
         for row in reader:
@@ -34,8 +37,9 @@ for fn in files:
             sur_name_dict[address] = sur_name
             given_name_dict[address] = given_name
 
+# output
 output_file = directory + '/main.csv'
 with open(output_file, 'w') as out:
     for address in total_dict.keys():
-        mm_string = '・'.join(total_dict[address])
+        mm_string = ''.join(total_dict[address])
         print(f'{sur_name_dict[address]},{given_name_dict[address]},{address},{mm_string}', file=out)
